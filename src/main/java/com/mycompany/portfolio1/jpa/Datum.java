9db1bbc77890be60527af39943a834a757667334
MildL <MildL@zo018-n01.marksys.de>
2018-02-26 10:20:24 +0100

    Klasse Datum wurde erstellt

---
+++ src/main/java/com/mycompany/portfolio1/jpa/Datum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.portfolio1.jpa;
+
+/**
+ *
+ * @author MildL
+ */
+public class Datum {
+    public int jahr;
+    public int monat;
+    public int tag;
+    
+    public Datum (int jahr, int monat, int tag)  {
+        this.jahr = jahr;
+        this.monat = monat;
+        this.tag = tag;
+    }
+    
+    @Override
+    public String toString(){
+        return (tag + "." + monat + "." + jahr);
+    }
+}
