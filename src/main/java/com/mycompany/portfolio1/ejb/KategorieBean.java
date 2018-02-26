d2dc6dc203ddab380a8d5647e418aaf0447144ef
MildL <MildL@zo018-n01.marksys.de>
2018-02-26 11:47:45 +0100

    Beans erstellt

---
+++ src/main/java/com/mycompany/portfolio1/ejb/KategorieBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.portfolio1.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author MildL
+ */
+@Stateless
+public class KategorieBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
